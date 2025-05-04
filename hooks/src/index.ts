dac4bf6f5a97a8c20b753fba5d8d4f9ba8da89d7
unknown <22raghvdevadiga@gmail.com>
2025-05-04 23:44:16 +0530

    prisma transaction

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,10 +1,32 @@
 import express from "express";
+import { PrismaClient } from '@prisma/client'
+
+const client = new PrismaClient()
 const app=express();
+
 
 app.post("/hooks/catch/:userId/:zapId",(req,res)=>{
     const userId=req.params.userId;
     const zapId=req.params.zapId;
+    const body=req.body;
+    client.$transaction(async(tx:any)=>{
+        const run= await tx.ZapRun.create({
+            data:{
+                zapId: zapId,
+                metadata: body
+            }
+        })
+        await tx.ZapRunOutbox.create({
+            data:{
+                zapRunId: run.id
+            }
+        })
+
+    })
+    res.json({ message: 'Webhook received' })
 
     
 })
-app.listen(3000);
+app.listen(3000 ,() => {
+    console.log('Server running on port 3002')
+});
